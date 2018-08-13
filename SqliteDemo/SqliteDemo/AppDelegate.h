fb32131f68d03992bc4b6fd887a2782e505d4cb7
OwenWang <839695819@qq.com>
2018-08-13 15:10:35 +0800

    Create Project

---
+++ SqliteDemo/SqliteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliteDemo
+//
+//  Created by admin on 2018/8/13.
+//  Copyright © 2018年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
